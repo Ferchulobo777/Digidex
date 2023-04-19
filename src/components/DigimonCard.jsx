51750c636fcccf1fb61c0f6e6053a24c7929af12
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-04-19 13:37:24 -0300

    update

--- src/components/DigimonCard.jsx
+++ src/components/DigimonCard.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const DigimonCard = ({ digimon }) => {
   return (
     <section className="mt-2 mb-2">
-      <div className="bg-orange-300 rounded-md p-6 mx-6 shadow-md shadow-black hover:shadow-lg hover:shadow-white">
+      <motion.div
+        className="bg-orange-300 rounded-md p-10 mx-6 shadow-md shadow-black"
+        whileHover={{
+          boxShadow: '0px 5px 15px -5px rgba(0, 0, 0, 0.3) inset',
+          scale: 1.05,
+          transition: { duration: 0.2 },
+        }}
+      >
         {digimon.img ? (
-          <img
+          <motion.img
             src={digimon.img}
             alt={digimon.name}
-            className="mx-auto hover:transform hover:scale-110 hover:saturate-200 hover:rounded-full border-8 border-cyan-300 outline outline-black"
+            className="mx-auto border-8 border-cyan-300 outline outline-black"
+            whileHover={{
+              scale: 1.1,
+              filter: 'saturate(200%)',
+              borderRadius: '50%',
+              transition: { duration: 0.2 },
+            }}
           />
         ) : null}
         <p className="text-xl font-black text-center capitalize mt-4">{digimon.name}</p>
         <p className="text-lg font-semibold text-center capitalize">{digimon.level}</p>
-      </div>
+      </motion.div>
     </section>
   );
 };
--- src/views/Digidex.jsx
+++ src/views/Digidex.jsx
@@ -2,7 +2,6 @@ import { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../context/UserContext';
 import axios from 'axios';
 import DigimonCard from '../components/DigimonCard';
-import Bylevel from '../components/ByLevel';
 import { usePagination } from '../hooks/usePagination';
 
 const Digidex = () => {
@@ -52,8 +51,26 @@ const Digidex = () => {
   };
 
   useEffect(() => {
-    loadAllDigimons();
-  }, [loadAllDigimons]);
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        let data = [];
+
+        if (selectedLevel === '') {
+          data = await getAllDigimons();
+        } else {
+          data = await getByLevel(selectedLevel);
+        }
+
+        setDigimons(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [selectedLevel]);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
@@ -101,8 +118,41 @@ const Digidex = () => {
           Inicio
         </button>
       </div>
-      <div className="flex w-full justify-center items-center">
-        <Bylevel getByLevel={getByLevel} />
+      <div className="flex w-3/4 md:w-1/2 justify-center items-center">
+        <div className="flex items-center justify-center w-full">
+          <label className="mr-2 font-bold text-xl">Nivel:</label>
+          <select
+            className="border border-black rounded-md p-2 text-center w-3/4 md:w-1/2 text-xl font-bold text-gray-500"
+            value={selectedLevel}
+            onChange={(e) => {
+              setSelectedLevel(e.target.value);
+              setSearchTerm('');
+              digimonsPagination.changePageTo(1);
+            }}
+          >
+            <option value="" className="font-black text-center text-zinc-900">
+              --Seleccione un nivel--
+            </option>
+            <option value="Fresh" className="font-black text-pink-300">
+              Fresh
+            </option>
+            <option value="In Training" className="font-black text-green-500">
+              In Training
+            </option>
+            <option value="Rookie" className="font-black text-blue-500">
+              Rookie
+            </option>
+            <option value="Champion" className="font-black text-red-500">
+              Champion
+            </option>
+            <option value="Ultimate" className="font-black text-violet-500">
+              Ultimate
+            </option>
+            <option value="Mega" className="font-black text-orange-500">
+              Mega
+            </option>
+          </select>
+        </div>
       </div>
       <div className="flex flex-wrap flex-row gap-4 mt-20 justify-center w-3/4 text-3xl font-bold hover:shadow-md hover:shadow-red-500 rounded-lg cursor-pointer h-20">
         {digimonsPagination.pages.map((page) => (
